36af6efb4fa165d115280beb29df2384739bdcbe
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-08 20:02:34 +0300

    Убрал неиспользуемый импорт

--- ads/views.py
+++ ads/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+# from django.shortcuts import render
 
 
-# Create your views here.
 def show_main_page(request):
     return HttpResponse('{"status": "ok"}')
