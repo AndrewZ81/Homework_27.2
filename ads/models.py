319d313997bce17ec9e9c6f2247fdf43d1a141c6
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-08 19:30:59 +0300

    Создал модели 'Advertisement' и 'Category'

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db.models import Model, \
+    CharField, PositiveIntegerField, BooleanField
 
-# Create your models here.
+
+class Advertisement(Model):
+    name = CharField(max_length=200)
+    author = CharField(max_length=100)
+    price = PositiveIntegerField()
+    description = CharField(max_length=2000)
+    address = CharField(max_length=200)
+    is_published = BooleanField()
+
+
+class Category(Model):
+    name = CharField(max_length=200)
