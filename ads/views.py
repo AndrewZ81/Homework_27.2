f1e2afa8a31caef263d99c92687b48f58a8ab0b8
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-09 17:58:30 +0300

    Создал представление show_categories

--- ads/views.py
+++ ads/views.py
@@ -1,6 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
 # from django.shortcuts import render
+from ads.models import Category
 
 
-def show_main_page(request):
+def show_main_page(request) -> HttpResponse:
     return HttpResponse('{"status": "ok"}')
+
+
+def show_categories(request) -> JsonResponse:
+    """
+    Отображает таблицу Category в формате списка словарей
+    :param request:
+    :return:
+    """
+    if request.method == "GET":
+        categories = Category.objects.all()
+        response: list = []
+        for category in categories:
+            response.append(
+                {
+                    "id": category.id,
+                    "name": category.name
+                }
+            )
+        return JsonResponse(response, safe=False,
+                            json_dumps_params={"ensure_ascii": False, "indent": 4})
