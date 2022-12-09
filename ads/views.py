75173162bc9e66dd17016a96ca73d644ba91a07d
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-09 18:08:47 +0300

    Создал представление show_advertisements

--- ads/views.py
+++ ads/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 
 # from django.shortcuts import render
-from ads.models import Category
+from ads.models import Category, Advertisement
 
 
 def show_main_page(request) -> HttpResponse:
@@ -22,6 +22,28 @@ def show_categories(request) -> JsonResponse:
                 {
                     "id": category.id,
                     "name": category.name
+                }
+            )
+        return JsonResponse(response, safe=False,
+                            json_dumps_params={"ensure_ascii": False, "indent": 4})
+
+
+def show_advertisements(request) -> JsonResponse:
+    """
+    Отображает таблицу Advertisement в формате списка словарей
+    :param request:
+    :return:
+    """
+    if request.method == "GET":
+        advertisements = Advertisement.objects.all()
+        response: list = []
+        for advertisement in advertisements:
+            response.append(
+                {
+                    "id": advertisement.id,
+                    "name": advertisement.name,
+                    "author": advertisement.author,
+                    "price": advertisement.price,
                 }
             )
         return JsonResponse(response, safe=False,
