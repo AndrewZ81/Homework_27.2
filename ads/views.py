f76f7cfb881f7306b6d9c4baac6f41fe694f54c0
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-10 18:52:53 +0300

    Обновил представление show_categories

--- ads/views.py
+++ ads/views.py
@@ -1,6 +1,11 @@
+import json
+from typing import List, Dict
+
 from django.http import HttpResponse, JsonResponse
 
 # from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from ads.models import Category, Advertisement
 
 
@@ -8,24 +13,36 @@ def show_main_page(request) -> HttpResponse:
     return JsonResponse({"status": "ok"})
 
 
+@csrf_exempt
 def show_categories(request) -> JsonResponse:
     """
-    Отображает таблицу Category в формате списка словарей
+    Отображает таблицу Category, делает выборку записи из Category по id
+    или создаёт новую запись Category
     :param request:
-    :return:
+    :return: Список словарей или словарь
     """
     if request.method == "GET":
         categories = Category.objects.all()
-        response: list = []
+        response_as_list: List[Dict[str, int | str]] = []
         for category in categories:
-            response.append(
+            response_as_list.append(
                 {
                     "id": category.id,
                     "name": category.name
                 }
             )
-        return JsonResponse(response, safe=False,
+        return JsonResponse(response_as_list, safe=False,
                             json_dumps_params={"ensure_ascii": False, "indent": 4})
+
+    elif request.method == "POST":
+        category_data = json.loads(request.body)
+        category: Category = Category(**category_data)
+        category.save()
+        response_as_dict: Dict[str, int | str] = {
+            "id": category.id,
+            "name": category.name
+        }
+        return JsonResponse(response_as_dict, json_dumps_params={"ensure_ascii": False, "indent": 4})
 
 
 def show_advertisements(request) -> JsonResponse:
